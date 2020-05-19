8187158dd660412dd78e9341c652327090bc2821
liyishen1988 <@@zz5544325>
2020-05-19 17:06:11 +0800

    多线程

---
+++ src/main/java/com/lys/main/多线程/MyThread.java
@@ -0,0 +1,13 @@
+package com.lys.main.多线程;
+
+/**
+ * Created by Administrator on 2020/5/18.
+ */
+public class MyThread implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/com/lys/main/多线程/SecondThread.java
@@ -0,0 +1,23 @@
+package com.lys.main.多线程;
+
+/**
+ * Created by Administrator on 2020/5/19.
+ */
+public class SecondThread implements Runnable{
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            SecondThread secondThread = new SecondThread();
+            new Thread(secondThread,"新线程1").start();
+            new Thread(secondThread,"新线程2").start();
+        }
+    }
+}
